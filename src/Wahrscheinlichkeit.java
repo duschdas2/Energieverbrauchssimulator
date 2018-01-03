ce49a310dc0da5809db11837da652e6008eae379
Julian <Julian@Julian.fritz.box>
2018-01-03 19:48:34 +0100

    - Pfad gefixt - Wahrscheinlichkeit implementierung begonnen

--- src/Wahrscheinlichkeit.java
+++ src/Wahrscheinlichkeit.java
@@ -1,4 +1,38 @@
+
 public class Wahrscheinlichkeit 
 {
-
+	int AktWahrscheinlichkeit = 0;
+	int x = 90; //Gerätelauf dauer muss von Gerät importiert werden
+	
+	public Wahrscheinlichkeit() {
+		
+	}	
+	
+	//Überpürft die Wahrscheinlichkeit eines bestimmten Zeitslots
+	//Benutzt zusätzlich Nachbarn
+	public void checkStatus(double [] statAnalysis,double[][] gerätAn,int auswertDaten,int geräte, int timeSlot) {
+		for(int i = 0;i<geräte;i++) {
+			if(timeSlot > 0) //Wenn nicht erstes Elelemnt
+			{
+				if(gerätAn[timeSlot-1][i] == 1) {	//Falls Gerät in vorherigen Element an war
+					//Benötigt check auf GeräteTyp und Gerät
+					//Variable je nach dem wie lange das Gerät an
+					AktWahrscheinlichkeit = +75;
+				}
+				else if(gerätAn[timeSlot-1][i] == 0) { //Falls Gerät nicht im vorherigen Element an war
+					if(statAnalysis[timeSlot-1] >= (auswertDaten/2)) { //Falls Gerät in statistischen Daten an war
+						AktWahrscheinlichkeit = +75;
+					}
+					else {
+						AktWahrscheinlichkeit = +25;
+					}
+				}
+			}
+			if(AktWahrscheinlichkeit >= 70) {
+				if(Math.random()*x >= 70) {
+					gerätAn[timeSlot][i] = 1;
+				}
+			}
+		}
+	}
 }
--- src/main.java
+++ src/main.java
@@ -11,19 +11,26 @@ import Haushalt.Personentype;
 public class main {
 
 	public static void main(String[] args) throws IOException {
-		double test[] = new double [1440];
 		//Geräte pc = new Geräte("pc1",100,10,1,2,1,1);
-		test = Einlesen.GetAll(10);
-		for(int i = 0;i<test.length;i++) {
-			System.out.println("Minute: "+i+ " : "+ test[i]);
+		int geräte = 1;
+		int auswertDaten = 10;
+		double [] statAnalysis = new double [1440];
+		double [][] gerätAn = new double [1440][geräte];
+		Wahrscheinlichkeit w1 = new Wahrscheinlichkeit();
+		
+		statAnalysis = Einlesen.GetAll(auswertDaten);
+		for(int i = 0;i<statAnalysis.length;i++) {
+			w1.checkStatus(statAnalysis,gerätAn,auswertDaten,geräte,i);
+			System.out.println(gerätAn[i][0]);
 		}
+		
 		Person person = new Person(15, new Personentype());
 		person.calcTime();
-		System.out.print(person);
+		//System.out.print(person);
 		ArrayList<Person> list = new ArrayList<Person>();
 		list.add(person);
 		Haushalt haushalt = new Haushalt(list);
 		haushalt.calcOccupancy();
-		System.out.print(haushalt);
+		//System.out.print(haushalt);
 	}
 }
