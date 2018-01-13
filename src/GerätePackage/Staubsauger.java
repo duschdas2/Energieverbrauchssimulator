2b1f89c7a7f478a5a0197b1ba66241c4e080528e
RevinKabe <kevin_rabe@gmx.de>
2018-01-13 19:00:39 +0100

    mini fix beim Staubsauger

--- src/GerätePackage/Staubsauger.java
+++ src/GerätePackage/Staubsauger.java
@@ -3,7 +3,7 @@ package GerätePackage;
 public class Staubsauger extends GeräteTyp3{
 	
 	public Staubsauger() {
-	super(690, 110, 0.2, 6.5, 0.001, 0.001, 125);
+	super(690, 110, 0.2, 6.5, 0.001, 0.001, 300);
 	this.aktuellerVerbrauch = this.maxVerbrauch;
 	}
 	
