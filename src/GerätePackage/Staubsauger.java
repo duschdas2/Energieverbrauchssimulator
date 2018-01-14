0d7eb4b34d054a6e9e33279787c16e96fc316f2c
RevinKabe <kevin_rabe@gmx.de>
2018-01-14 15:14:34 +0100

    Staubsauger Verbrauchsänderungswahrscheinlichkeit angepasst

--- src/GerätePackage/Staubsauger.java
+++ src/GerätePackage/Staubsauger.java
@@ -3,7 +3,7 @@ package GerätePackage;
 public class Staubsauger extends GeräteTyp3{
 	
 	public Staubsauger() {
-		super(690, 110, 0.2, 6.5, 0.001, 0.001, 300);
+		super(690, 110, 0.2, 6.5, 0.002, 0.002, 125);
 		this.aktuellerVerbrauch = this.maxVerbrauch;
 	}
 
